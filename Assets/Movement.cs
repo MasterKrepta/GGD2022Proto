28e2c936de338ee7bcbc228d9d6b49c513f3a590
Brian <brianj.pajak@gmail.com>
2022-01-27 20:29:15 -0500

    Dual screen movement and pickups

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float movespeed;
+    
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -15,17 +16,14 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float move_x = Input.GetAxis("Horizontal");
-        float move_z = Input.GetAxis("Vertical");
-        Vector3 Up = new Vector3(0f, rb.velocity.y, 0f);
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        rb.velocity = (transform.forward * move_z + transform.right * move_x).normalized * movespeed + Up;
+        rb.velocity = movement * movespeed;
 
-
-
-        //Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-
-        //rb.velocity = movement * movespeed;
-        //transform.LookAt(movement);
+        if (movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(movement);
+        }
+        
     }
 }
